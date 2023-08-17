63baba4d0521a404475e52c10a23112b2e7ae0f2
liuning <1075858260@qq.com>
2023-08-17 17:20:22 +0800

    修改测试项目

--- pack包加密研究/Program.cs
+++ pack包加密研究/Program.cs
@@ -5,7 +5,6 @@ using KYSharp.SM;
 
 Console.WriteLine("Hello, World!");
 
-
 string jdata = "json数据1";
 string guid1 = "e4dd81a3946e4cd8b6fe737481530a60";
 string md51 = "5778eb2fd4c2810f8d5010802450061c";
@@ -25,45 +24,7 @@ else
 }
 
 
-List<NormalDataModel> lstNormal = new List<NormalDataModel>() {
-     new (){ Data = "json数据1", UserId  ="u1"},
-     new (){ Data = "json数据2", UserId  ="u2"},
-     new (){ Data = "json数据3", UserId  ="u3"}
-};
-
-List<PackDatModel> packDats = new()
-{
-     new (){ Id ="e4dd81a3946e4cd8b6fe737481530a60",Md5 =  "5778eb2fd4c2810f8d5010802450061c"}
-};
-
-//转包数据存凭证
-string guid =  Guid.NewGuid().ToString("N").ToLower();
-string?  dMd5 = md5(lstNormal[0].Data+guid);
-packDats.Add(new PackDatModel { Md5 = dMd5, Id = guid});
-
-//保存实体
-using (StreamWriter sw = new StreamWriter(@$"D:\ln2023\PrivateProject\commonWeb\target\{dMd5}.mcm.pack", false, System.Text.Encoding.UTF8))
-{
-    sw.Write(lstNormal[0].Data);
-}
-
-
-Console.ReadLine();
-
 string md5(string val)
 {
    return string.Join("", MD5.Create().ComputeHash(Encoding.UTF8.GetBytes(val)).Select(x => x.ToString("x2")));
-}
-
-
-class PackDatModel
-{
-    public string Id { get; set; }
-    public string Md5 { get; set; }
-}
-
-class NormalDataModel
-{
-    public string Data { get; set; }
-    public string UserId { get; set; }
 }
